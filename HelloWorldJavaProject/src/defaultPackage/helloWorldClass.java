de622cfff4326bd66ba5f613fd35f43d6b468f73
Karthik Govinda Raju <karthikgre273@gmail.com>
2020-05-25 23:58:34 -0700

    Added a new Hello World Java Project

---
+++ HelloWorldJavaProject/src/defaultPackage/helloWorldClass.java
@@ -0,0 +1,9 @@
+package defaultPackage;
+public class helloWorldClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
